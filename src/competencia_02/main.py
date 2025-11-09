f5913752e398a6cbe6586e69f12c3f4ae76a484e
nicolascaricati2 <nicolascaricati.educ@gmail.com>
2025-11-09 17:03:04 +0000

    Actualizo main y opt

--- src/competencia_02/main.py
+++ src/competencia_02/main.py
@@ -133,7 +133,7 @@ def main():
         #     df_fe = feature_engineering_lag(df_fe, columnas=atributos, cant_lag=i)
 
         df_fe = generar_cambios_de_pendiente_multiples_fast(df_fe, columnas=columnas_para_fe_regresiones, ventana_corta=3, ventana_larga=6)
-        # df_fe = generar_cambios_de_pendiente_multiples(df_fe, columnas=columnas_para_fe_regresiones, ventana_corta=6, ventana_larga=12)
+        df_fe = generar_cambios_de_pendiente_multiples_fast(df_fe, columnas=columnas_para_fe_regresiones, ventana_corta=6, ventana_larga=12)
 
         # for i in (2,3,6,8,10,12,15):
         #     df_fe = feature_engineering_regr_slope_window(df_fe, columnas=columnas_para_fe_regresiones, ventana = i)
@@ -141,8 +141,8 @@ def main():
         for i in (2,3):
             df_fe = feature_engineering_delta(df_fe, columnas=columnas_para_fe_deltas, cant_delta = i)
         for i in (4,8):
-            df_fe = feature_engineering_delta_max(df_fe, columnas=columnas_para_deltas, ventana=i)
-            df_fe = feature_engineering_delta_mean(df_fe, columnas=columnas_para_deltas, ventana=i)
+            df_fe = feature_engineering_delta_max(df_fe, columnas=columnas_para_fe_deltas, ventana=i)
+            df_fe = feature_engineering_delta_mean(df_fe, columnas=columnas_para_fe_deltas, ventana=i)
         
         df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})  
 
