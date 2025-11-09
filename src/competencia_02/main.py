1cccf393e8fb6fde3c4c2de06d5e8bbc6223866a
nicolascaricati2 <nicolascaricati.educ@gmail.com>
2025-11-09 05:02:26 +0000

    Actualizo main y opt

--- src/competencia_02/main.py
+++ src/competencia_02/main.py
@@ -217,6 +217,7 @@ def main():
     #06 Entrenar modelo final
     logger.info("=== ENTRENAMIENTO FINAL ===")
     logger.info("Preparar datos para entrenamiento final")
+    X_train, y_train, X_predict, clientes_predict = preparar_datos_entrenamiento_final(df_fe)
   
     # Entrenar modelo final
     logger.info("Entrenar modelo final")
