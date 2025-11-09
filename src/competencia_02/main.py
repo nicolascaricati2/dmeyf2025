57dbd9046a56b7c9ca46dbf5358fa13775235340
nicolascaricati2 <nicolascaricati.educ@gmail.com>
2025-11-09 19:31:31 +0000

    Actualizo main y opt

--- src/competencia_02/main.py
+++ src/competencia_02/main.py
@@ -89,9 +89,9 @@ def main():
 
         # 1. Undersampling
         df_fe = convertir_clase_ternaria_a_target(df)
-        df_fe = df_fe[df_fe["target"].notnull()].copy()
-        df_fe = undersample_clientes(df_fe, UNDERSAMPLING, 555557)
-        logger.info(f"Después de undersampling: {df_fe.shape}")
+        # df_fe = df_fe[df_fe["target"].notnull()].copy()
+        # df_fe = undersample_clientes(df_fe, UNDERSAMPLING, 555557)
+        # logger.info(f"Después de undersampling: {df_fe.shape}")
 
 
 
@@ -119,13 +119,13 @@ def main():
         columnas_a_excluir = ["foto_mes","cliente_edad","numero_de_cliente","target","target_to_calculate_gan"]
         columnas_para_fe_regresiones = [
             c for c in df_fe.columns
-            if c.startswith(('m', 'Visa_m', 'Master_m')) 
+            if c.startswith(('m', 'Visa_m', 'Master_m','TC_Total_m')) 
             and c not in columnas_a_excluir
         ]
         
         columnas_para_fe_deltas = [
             c for c in df_fe.columns
-            if c.startswith(('c', 'Visa_c', 'Master_c')) 
+            if c.startswith(('c', 'Visa_c', 'Master_c','Master_s','Visa_s','TC_Total_c','TC_Total_s','t' )) 
             and c not in columnas_a_excluir
         ]
         df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
