eb999a0dc3c1356369efb27d4a45b8e66c521ad9
nicolascaricati2 <nicolascaricati.educ@gmail.com>
2025-11-20 22:19:16 +0000

    Actualizo main y opt

--- src/competencia_03/main.py
+++ src/competencia_03/main.py
@@ -71,7 +71,7 @@ def main():
     crear_snapshot_modelo(STUDY_NAME)
 
     # path_parquet = os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.parquet")
-    path_parquet = "../../../buckets/b1/Compe_02/data/df_feModelo Final US 0.2 - Uso Meses Corridos y seasonality - Cambio IPC por Percentiles Rank V3.parquet")
+    path_parquet = "../../../buckets/b1/Compe_02/data/df_feModelo Final US 0.2 - Uso Meses Corridos y seasonality - Cambio IPC por Percentiles Rank V3.parquet"
 
     if os.path.exists(path_parquet):
         logger.info("✅ df_fe.parquet encontrado")
